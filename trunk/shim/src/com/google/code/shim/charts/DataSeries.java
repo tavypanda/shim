9c48d46930f5660e3d5036a94cd75604a07ce879
derek.gau@gmail.com <derek.gau@gmail.com@e02c682d-5a29-8336-b031-41f3c56f50bf>
2012-03-10 19:21:06 +0000

    Added google charts dataseries class, plus ApplicationContextProvider class for spring framework context access.  Added spring framework dependencies on core, context, and beans

---
+++ trunk/shim/src/com/google/code/shim/charts/DataSeries.java
@@ -0,0 +1,165 @@
+package com.google.code.shim.charts;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ * Convenience class that wraps a numeric data series, making it easier to work with the GoogleCharts API.
+ * @author dgau
+ *
+ */
+public class DataSeries {
+	static Logger logger = LogManager.getLogger(DataSeries.class);
+	
+	private int pctMin;
+	private int pctMax;
+	private int pctAvg;
+	
+	private Number min;
+	private Number max;
+	private Number avg;
+	
+	private final String name;
+	private final Number[] series;
+	private Integer[] pctSeries;
+	public DataSeries(String n, Number[] s){
+		name = n;
+		series = s;
+		
+		if(series!=null && series.length!=0){
+			Number[] minMaxAvg = findMinMaxAvg(series);
+			min=minMaxAvg[0];
+			max=minMaxAvg[1];
+			avg=minMaxAvg[2];
+			
+			pctSeries=normalizeToPercentages(series);
+			
+			Number[] pctMinMaxAvg = findMinMaxAvg(pctSeries);
+			pctMin=Math.round(pctMinMaxAvg[0].floatValue());
+			pctMax=Math.round(pctMinMaxAvg[1].floatValue());
+			pctAvg=Math.round(pctMinMaxAvg[2].floatValue());
+		}
+	}
+	
+
+	/**
+	 * @return the pctMin
+	 */
+	public int getPctMin() {
+		return pctMin;
+	}
+
+
+	/**
+	 * @return the pctMax
+	 */
+	public int getPctMax() {
+		return pctMax;
+	}
+
+
+	/**
+	 * @return the pctAvg
+	 */
+	public int getPctAvg() {
+		return pctAvg;
+	}
+
+
+	/**
+	 * @return the min
+	 */
+	public Number getMin() {
+		return min;
+	}
+
+
+	/**
+	 * @return the max
+	 */
+	public Number getMax() {
+		return max;
+	}
+
+
+	/**
+	 * @return the avg
+	 */
+	public Number getAvg() {
+		return avg;
+	}
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+	/**
+	 * @return the raw numeric series
+	 */
+	public Number[] getSeries() {
+		return series;
+	}
+
+
+	/**
+	 * @return the series normalized to whole integer percentages (for chart display)
+	 */
+	public Integer[] getPctSeries() {
+		return pctSeries;
+	}
+
+
+	private static Number[] findMinMaxAvg( Number[] s) {
+		Double min = Double.MAX_VALUE;
+		Double max = -1*Double.MAX_VALUE;
+		Double sum = 0.0;
+		int points = 0;
+		for (int i = 0; i < s.length; i++) {
+			if (s[i] == null)
+				continue;
+			points++;
+			if (min == null || s[i].doubleValue() < min ) {
+				min = s[i].doubleValue();
+			}
+			if (max == null || s[i].doubleValue() > max ) {
+				max = s[i].doubleValue();
+			}
+			sum = sum + s[i].doubleValue();
+		}
+		double avg = points == 0 ? 0 : sum / points;
+		return new Number[]{min,max,avg};
+	}
+	/**
+	 * Produces an integer series of by normalizing the incoming series into rounded percentages (no decimals)
+	 * @param series
+	 * @return
+	 */
+	private static Integer[] normalizeToPercentages(Number[] series) {
+		Number[] minMaxAvg = findMinMaxAvg(series);
+		
+		if (minMaxAvg[1].doubleValue() == 0.0) {
+			logger.warn("Data series has a max value = 0, cannot convert to percentages.");
+			return new Integer[0];
+		}
+
+		Integer[] results = new Integer[series.length];
+		for (int i = 0; i < series.length; i++) {
+			if (series[i] == null) {
+				results[i]=null;
+			} else {
+				results[i]=(int) Math.round(series[i].floatValue()/minMaxAvg[1].floatValue()*100);
+			}
+		}
+		
+		return results;
+		
+	}
+	 
+	
+	
+}
