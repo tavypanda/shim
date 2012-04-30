a73e1b36c3c9414206c901a0c336bb7976bc91ad
derek.gau@gmail.com <derek.gau@gmail.com@e02c682d-5a29-8336-b031-41f3c56f50bf>
2012-04-30 14:24:23 +0000

    Removed dependencies on StringKeyMap, which is now deprecated.

--- trunk/shim/src/com/google/code/shim/data/sql/handler/RowHandler.java
+++ trunk/shim/src/com/google/code/shim/data/sql/handler/RowHandler.java
@@ -1,21 +1,20 @@
 package com.google.code.shim.data.sql.handler;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.apache.commons.dbutils.BasicRowProcessor;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.RowProcessor;
 
-import com.google.code.shim.collections.StringKeyMap;
-
 /**
- * <code>ResultSetHandler</code> implementation that converts the first
- * <code>ResultSet</code> row into a <code>StringKeyMap</code>. 
- *
+ * <code>ResultSetHandler</code> implementation that converts the first <code>ResultSet</code> row into a
+ * <code>Map<String,Object></code>.
+ * 
  * @see org.apache.commons.dbutils.ResultSetHandler
  */
-public class RowHandler implements ResultSetHandler<StringKeyMap> {
+public class RowHandler implements ResultSetHandler<Map<String, Object>> {
 
 	/**
 	 * The RowProcessor implementation to use when converting rows into Maps.
@@ -26,17 +25,15 @@ public class RowHandler implements ResultSetHandler<StringKeyMap> {
 		super();
 		convert = new OrderedBasicRowProcessor();
 	}
-	
+
 	public RowHandler(RowProcessor r) {
 		super();
 		convert = r;
 	}
 
 	@Override
-	public StringKeyMap handle(ResultSet rs) throws SQLException {
-		return  rs.next() ?  new StringKeyMap(convert.toMap(rs)) : null; 
+	public Map<String, Object> handle(ResultSet rs) throws SQLException {
+		return rs.next() ? new LinkedHashMap<String, Object>(convert.toMap(rs)) : null;
 	}
-
-	
 
 }
